e9ea0304aada58480f1d46eb5a09f9a297964480
Игорь Краснюков <digenets@gmail.com>
2020-10-26 11:45:31 +0300

    commit 1st

--- Contest2/Source.cpp
+++ Contest2/Source.cpp
@@ -1,45 +1,113 @@
+#define f1
+#ifdef f1
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 #define SIZE 101
+
+int modulo(int dividend, int divisor)
+{
+	return (divisor + (dividend % divisor)) % divisor;
+}
 
 int main()
 {
 	int N = 0, M = 0;
 	int arr1[SIZE][SIZE] = { 0 }, arr2[SIZE][SIZE] = { 0 };
 	scanf("%d%d", &N, &M);
-	for (int i = 1; i < N+1; i++)
+	// ввод массива
+	for (int n = 0; n < N; n++)
 	{
-		for (int j = 1; j < M+1; j++)
+		for (int m = 0; m < M; m++)
 		{
-			scanf("%d", &arr1[i][j]); // Массив введён
+			scanf("%d", &arr1[n][m]); // Массив введён
+		}
+	}
+	// приравнивание массивов
+	for (int n = 0; n < N; n++)
+	{
+		for (int m = 0; m < M; m++)
+		{
+			arr2[n][m] = arr1[n][m];
 		}
 	}
 
-	int i = 0, moved = 1, countCh = 1;
-	while ((i < 1000) && (moved != 0) && (countCh != 0))
+	int i = 0, alive = 1, countCh = 1;
+	while ((i < 1000) && (alive != 0) && (countCh != 0))
 	{
+		alive = 0;
+		countCh = 0;
 		for (int n = 0; n < N; n++)
 		{
 			for (int m = 0; m < M; m++)
 			{
 				int sum = 0;
-				sum = arr1[(n-1) % N][m] + arr1[(n+1)%N][m] + arr1[n][(m-1) % M] + arr1[n][(m+1) % M] + arr1[(n+1)%N][(m+1)%M] + arr1[(n+1)%N][(m-1)%M] + arr1[(n-1)% N][(m+1)%M] + arr1[(n-1)%N][(m-1)% M];
-				if ((sum >= 3) && (arr1[n][m] != 1))
+				sum = arr1[n][modulo(m + 1, M)] + arr1[n][modulo(m - 1, M)] +
+					  arr1[modulo(n + 1, N)][m] + arr1[modulo(n - 1, N)][m] +
+					  arr1[modulo(n + 1, N)][modulo(m + 1, M)] +
+					  arr1[modulo(n + 1, N)][modulo(m - 1, M)] +
+					  arr1[modulo(n - 1, N)][modulo(m + 1, M)] +
+					  arr1[modulo(n - 1, N)][modulo(m - 1, M)];
+				if ((sum == 3) && (arr1[n][m] != 1))
 				{
-					arr1[n][m] = 1;
-
+					arr2[n][m] = 1;
+					countCh++;
+					
+				}
+				if ((sum != 2) && (sum != 3) && (arr1[n][m] == 1))
+				{
+					arr2[n][m] = 0;
+					countCh++;
+				}
+				if (arr1[n][m] == 1)
+				{
+					alive++;
 				}
 			}
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < M; j++)
+				{
+					arr1[i][j] = arr2[i][j];
+				}
+			}
+			for (int n = 0; n < N; n++)
+			{
+				for (int m = 0; m < M; m++)
+				{
+					if (arr2[n][m] == 1)
+						printf("x");
+					else
+						printf(" ");
+
+				}
+				
+
+			}
 		}
+		/*if (((i == 0) ||(i == 9) || (i == 99) || (i == 999)) && (alive != 0) && (countCh != 0))
+		{
+			printf("Step %d: alives %d\n", i + 1, alive);
+			for (int n = 0; n < N; n++)
+			{
+				for (int m = 0; m < M; m++)
+				{
+					if (arr1[n][m] == 1)
+						printf("x");
+					else
+						printf(" ");
+					
+				}
+				printf("\n");
+			}
+		}*/
 		i++;
 	}
+	printf("End life step %d", i);
 
 
 
-
-
-
+	printf("xuy");
 	//for (int c = 0; c < 1000; c++)
 	//{
 	//	/*for (int j = 2; j < M - 1; j++)
@@ -63,18 +131,6 @@ int main()
 	//	arr1[0][M] = arr1[N - 1][1];
 	//	arr1[N][0] = arr1[1][M - 1];*/
 
-	//	for (int i = 0; i < N; i++)
-	//	{
-	//		for (int j = 0; j < M; j++)
-	//		{
-
-	//		}
-	//	}
-
-	//	
-	//}
-
-
-
 	return 0;
 }
+#endif f1
